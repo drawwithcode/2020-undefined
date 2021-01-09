b120ff1239a1a001351879907af1c34ce99d12e2
tolbri <65823209+tolbri@users.noreply.github.com>
2021-01-09 13:21:38 +0100

    Added data to map

--- sketch.js
+++ sketch.js
@@ -1,48 +1,57 @@
-let map;
-
+let myMap;
 let canvas;
-
 const mappa = new Mappa('Leaflet');
 
-// Lets put all our map options in a single object
-const options = {
-  lat: 45.4642,
-  lng: 9.19000,
+const mapOptions = {
+  lat: 45.4642, //Latitude of Milan
+  lng: 9.19000, //Longitude of Milan
   zoom: 13,
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
-function preload(){
+// This is for testing purpose.
+// The actual data will come from the database.
+const flowers = [{
+    "name": "flower_1",
+    "lat": 45.4642,
+    "lng": 9.19000
+  },
+  {
+    "name": "flower_2",
+    "lat": 45.4645,
+    "lng": 9.22000
+  }
+]
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
   canvas = createCanvas(640, 640);
 
-  // Create a tile map with lat 0, lng 0, zoom 4
- map = mappa.tileMap(options);
- // Overlay the canvas over the tile map
- map.overlay(canvas);
-
- fill(200, 100, 100);
-
- // Only redraw the point when the map changes and not every frame.
-  map.onChange(drawPoint);
-
+  myMap = mappa.tileMap(mapOptions);
+  myMap.overlay(canvas);
+  myMap.onChange(drawFlowers);
 }
 
 function draw() {
 
-
 }
 
-function drawPoint() {
+function drawFlowers() {
   clear();
-  // put drawing code here
-  // Every Frame, get the canvas position
-// for the latitude and longitude of Nigeria
-const nigeria = map.latLngToPixel(11.396396, 5.076543);
-// Using that position, draw an ellipse
-ellipse(nigeria.x, nigeria.y, 20, 20);
-
+  // Get the coordinates for every object in the flowers-array
+  for (let i = 0; i < flowers.length; i++) {
+    const latitude = Number(flowers[i].lat);
+    const longitude = Number(flowers[i].lng);
+    // Only draw the objects that are within the canvas
+    if (myMap.map.getBounds().contains({
+        lat: latitude,
+        lng: longitude
+      })) {
+      const pos = myMap.latLngToPixel(latitude, longitude);
+      ellipse(pos.x, pos.y, 20);
+    }
+  }
 }
