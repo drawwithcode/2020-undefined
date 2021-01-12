af9111549c1d348c2b173a7bb3e6a28eb03e548c
aliakaouel <31700227+aliakaouel@users.noreply.github.com>
2021-01-12 17:29:25 +0100

    TRYING POPUP

---
+++ public/popups.js
@@ -0,0 +1,11 @@
+document.getElementById('clickPlant').addEventListener('click',
+function() {
+  document.querySelector('.infoplant').style.display = 'flex';
+});
+
+
+
+function myFunction() {
+  let popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
